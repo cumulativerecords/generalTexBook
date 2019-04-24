a57e4fd4538f16b27ed2fbc1b49241c3d8cc092b
B. Theisen <17404276+doctortheisen@users.noreply.github.com>
2019-04-24 05:03:26 -0700

    Create taskListVanilla.h

---
+++ sections/taskListVanilla.h
@@ -0,0 +1,8 @@
+/* Example header file for generating a latex file of a task list in C */
+
+Vanilla = { “Concept”,
+            “Examples”,
+            “Literature”,
+            “Related Task List Items”
+           };
+
